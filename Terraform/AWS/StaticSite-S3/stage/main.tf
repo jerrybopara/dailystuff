8d2028b3d66fed616504d2294df0398e9cca510b
Jerry Bopara <jerrybopara@gmail.com>
2022-05-07 23:38:32 +0530

    update

--- Terraform/AWS/StaticSite-S3/stage/main.tf
+++ Terraform/AWS/StaticSite-S3/stage/main.tf
@@ -5,13 +5,12 @@ terraform {
       version = "~> 3.27"
     }
   }
-
   required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  profile = "ir-jerry-test"
-  region  = "us-east-1"
+  profile = var.aws_profile
+  region  = var.aws_region
 }
 
 resource "aws_instance" "jerry_app_server" {
@@ -20,7 +19,7 @@ resource "aws_instance" "jerry_app_server" {
   key_name      = "IR-PMTA-AWS"
 
   tags = {
-    "Name" = "Example_jerry_app_server"
+    "Name" = var.instance_name
   }
 
 }
