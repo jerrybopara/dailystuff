7c806c435dffcbd9a0f1c866d3650c0956664eaf
Jerry Bopara <jerrybopara@gmail.com>
2022-05-28 15:12:15 +0530

    update

--- Terraform/AWS/Launch_Ec2/main.tf
+++ Terraform/AWS/Launch_Ec2/main.tf
@@ -24,6 +24,7 @@ module "my_vpc" {
 
 module "ec2_Module" {
   source        = "./modules/ec2"
+  infra_env     = var.infra_env
   count         = var.ec2_count
   instance_name = var.instance_name
   instance_type = var.instance_type
