9d45f07131bae5d18fc70b933c4bcf4da3dfe122
Jerry Bopara <jerrybopara@gmail.com>
2022-05-28 14:51:15 +0530

    update

--- Terraform/AWS/Launch_Ec2/modules/ec2/ec2.tf
+++ Terraform/AWS/Launch_Ec2/modules/ec2/ec2.tf
@@ -3,7 +3,14 @@ resource "aws_key_pair" "loginkey" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCusc/nK5sEyn4rPCBS4IriFXm27I43JTrWhrpUn2JQDP5olbjWS35JGCZE8mc1N2Un4S3+/73IgwHqIGBhA4HsSWxnabIrJDKeBdWjMQByg3wLKOFUogp6sy86yszpAvUtOxdpNxNUymFUyb75gZZqTlgywb0s82d2iDIgs3dKZsNILdVOUSetAtSqbGv/MGtdCvz62PbCIFyKRkpW4Fep/oKzCk/bZAtx3ItlIZE4ZDeOT0CJ4tWNjKKvJDNGQoj5Hj6gqG5ih4XZqz2wjJpcQpDE8uVyb8xBEcUDZbxlbiMPEY1D4EXBVbSqUuVquFH1ELyLvRz4tVYYJQK2bQe51oBPqC7pbyWW7wdb+qm2vwXa8kii+PHiKdG6ddfXdTVktV5moo1AbiRj9BBxzwx4SMkWPb5RuhF6qJvPO3rEtQDB4leT5SAHQ1u0zrvRg8QvEs9QGZZL2+Mhn3V4ggQ8kqwYremjYbDqS8E4sw2cbKgjDNR22Vdwykdou7Aunrk= jerry@ideapad"
 }
 
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_instance" "instance" {
+  # Choosing the AZ - us-east-1e
+  availability_zone = data.aws_availability_zones.available.names[5]
   count           = var.ec2_count
   ami             = var.ami_id
   instance_type   = var.instance_type
